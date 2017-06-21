8a121ec90a5d2bff2e1ac3c43117ab759ce3e275
Katie Jiang <katiejiang@fb.com>
2017-06-21 11:27:19 -0700

    Hooked up the Tumblr API

--- Tumblr Feed/FeedViewController.swift
+++ Tumblr Feed/FeedViewController.swift
@@ -9,11 +9,35 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    var posts: [[String: Any]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default,    delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                // TODO: Get the posts and store in posts property
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                
+                // TODO: Reload the table view
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
